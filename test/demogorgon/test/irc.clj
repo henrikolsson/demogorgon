2a139b731ffafdf8b2e295fc4a62ef90e4197072
Henrik Olsson <henrik@fixme.se>
2010-09-13 18:41:58 +0200

    Fixed irc tests

--- test/demogorgon/test/irc.clj
+++ test/demogorgon/test/irc.clj
@@ -3,18 +3,14 @@
   (:use [clojure.test]))
 
 (deftest can-parse-line
-  (is (= {:prefix nil
-          :command "TEST"
-          :args ["a" "b"]} (parse-line "TEST a b")))
-  (is (= {:prefix "test.example.com"
-          :command "372"
-          :args ["foo bar"]} (parse-line ":test.example.com 372 :foo bar")))
-  (is (= {:prefix "someone!foo@example.com"
-          :command "PRIVMSG"
-          :args ["me" "hey you"]} (parse-line ":someone!foo@example.com PRIVMSG me :hey you")))
-  (is (= {:prefix "someone!foo@example.com"
-          :command "PRIVMSG"
-          :args ["me" "hey you"]} (parse-line ":someone!foo@example.com PRIVMSG me :hey you"))))
-
+  (is (=
+       {:prefix {:nick nil, :username nil, :host nil}, :command "TEST", :args ["a" "b"]}
+       (parse-line "TEST a b")))
+  (is (=
+       {:prefix {:nick nil, :username nil, :host "test.example.com"}, :command "372", :args ["foo bar"]}
+       (parse-line ":test.example.com 372 :foo bar")))
+  (is (=
+       {:prefix {:nick "someone", :username "foo", :host "example.com"}, :command "PRIVMSG", :args ["me" "hey you"]}
+       (parse-line ":someone!foo@example.com PRIVMSG me :hey you"))))
 
 
