f2d176ceae32aef883aba441420026ecbc10cf38
Henrik Olsson <henrik@fixme.se>
2011-01-02 02:46:19 +0100

    New algorithm for searching unicode database. Also fix for codepoints above BMP.

--- src/demogorgon/unicode.clj
+++ src/demogorgon/unicode.clj
@@ -1,28 +1,43 @@
 (ns demogorgon.unicode
-  (:use	[clojure.contrib.duck-streams :only (reader)]))
+  (:use	[clojure.contrib.duck-streams :only (reader read-lines)]))
 
 (defn get-resource [name]
   (.getResourceAsStream (.getContextClassLoader (Thread/currentThread)) name))
 
+(defn unicode-matches? [needle name codepoint]
+  (or (> (.indexOf (.toLowerCase name) needle) -1)
+      (= needle (.toLowerCase codepoint))))
+    
 (defn unicode-lookup [_symbol]
   (let [symbol (.toLowerCase _symbol)]
-    (with-open [rdr (reader (get-resource "unicode.txt"))]
-      (let [lines (line-seq rdr)]
-        (some (fn [^String line]
-                (let [tokens (.split line "\t")
-                      ^String name (.trim (aget tokens 0))
-                      ^String codepoint (.trim (aget tokens 1))]
-                  (if (or (= symbol (.toLowerCase name))
-                          (= symbol (.toLowerCase codepoint)))
-                    {:name name :codepoint codepoint}
-                    nil))) lines)))))
+    (let [matches
+          (filter (fn [^String line]
+                    (let [tokens (.split line "\t")
+                          ^String name (.trim (aget tokens 0))
+                          ^String codepoint (.trim (aget tokens 1))]
+                      (unicode-matches? symbol name codepoint)))
+                  (read-lines (get-resource "unicode.txt")))]
+      (sort-by (fn [item]
+                 (- (count (:name item)) (count symbol)))
+               (map (fn [^String line]
+                      (let [tokens (.split line "\t")
+                            ^String name (.trim (aget tokens 0))
+                            ^String codepoint (.trim (aget tokens 1))]
+                        {:name name :codepoint codepoint})) matches)))))
 
 (defn unicode-hook [irc object match]
   (if (= (second match) "")
     (str "usage: " (first match) " <symbol>")
-    (let [result (unicode-lookup (second match))]
-      (if result
-        (str "U+" (:codepoint result) " " (:name result)
-             " (" (char (Integer/valueOf (:codepoint result) 16)) ")")
-        "No match"))))
+    (if (< (count (second match)) 3)
+      (str "minimum search length is 3")
+      (let [result (take 5 (unicode-lookup (second match)))]
+        (if (first result)
+          
+          (map (fn [foo]
+                 (str "U+" (:codepoint foo) " " (:name foo)
+                      " (" (String. (Character/toChars (Integer/valueOf (:codepoint foo) 16))) ")"))
+               result)
+          "No match")))))
+
+
 
