43f6e0bce638b771a17805b59d52c0f226428443
Henrik Olsson <henrik@fixme.se>
2012-02-27 18:47:57 +0100

    Label project as 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject demogorgon "1.0.0-SNAPSHOT"
+(defproject demogorgon "1.0.0"
   :description "demogorgon"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
