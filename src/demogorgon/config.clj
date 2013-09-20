cd7c1556ed5cc3bfa3f022e9f344018fde39e3bb
Henrik Olsson <henrik@fixme.se>
2013-09-21 00:48:01 +0200

    whoops

--- src/demogorgon/config.clj
+++ src/demogorgon/config.clj
@@ -2,10 +2,10 @@
 
 (def config
      {:un-dir "/opt/nethack.nu/var/unnethack/"
-      :twitter {:consumer_key "reEFD0AjpXtn7mJ6MVlX1g"
-                :consumer_secret "Nij9GdGR58VRDR5dbTqQin97HnhgdXwogY6lnrKxSrs"
-                :oauth_token "179042090-SNSfPwsE2EIKTAUPicBLKaccdbBkgoSt5y737FsY"
-                :oauth_token_secret "SqskJ8PTPXfMCwMxwioJRcpHGGNutC7xsgfeBmR9rMU"}
+      :twitter {:consumer_key ""
+                :consumer_secret ""
+                :oauth_token ""
+                :oauth_token_secret ""}
       :irc {:nick "demogorgon"
             :username "demogorgon"
             :realname "demogorgon"
