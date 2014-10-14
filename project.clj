423e57ea242f7de76436633a86dd1cfc3f3b4a8b
Henrik Olsson <henrik@fixme.se>
2014-10-14 18:57:33 +0200

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject demogorgon "1.1.2-SNAPSHOT"
   :description "demogorgon"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [org.apache.mina/mina-core  "2.0.7"]
                  [ring "1.2.0"]
                  [compojure "1.1.5"]
@@ -12,7 +13,9 @@
                  [org.twitter4j/twitter4j-core "3.0.3"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
                  [org.clojure/data.json "0.2.5"]
-                 [postgresql/postgresql "9.1-901-1.jdbc4"]]
+                 [postgresql/postgresql "9.1-901-1.jdbc4"]
+                 [org.clojure/tools.nrepl "0.2.3"]]
+
   :profiles
   {:start
    {:repl-options 
